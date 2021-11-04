d9193c12749b53bafc490ac6b450335f95385c17
ilyass asrarfi <aae.ilyass1890@gmail.com>
2021-11-04 22:47:58 +0100

    PROJECT

---
+++ script/script.js
@@ -0,0 +1,64 @@
+function felter1(){
+    document.getElementById('piza1').style.display="inline-block";
+    document.getElementById('piza2').style.display="inline-block";
+    document.getElementById('piza3').style.display="inline-block";
+    document.getElementById('piza4').style.display="inline-block";
+    // --------------------------------------------------
+    document.getElementById('passta1').style.display="inline-block";
+    document.getElementById('passta2').style.display="inline-block";
+    document.getElementById('passta3').style.display="inline-block";
+    // --------------------------------------------------
+    document.getElementById('sand1').style.display="inline-block";
+    document.getElementById('sand2').style.display="inline-block";
+    document.getElementById('sand3').style.display="inline-block";
+
+}
+
+
+// pizza
+function felter2(){
+    document.getElementById('passta1').style.display="none";
+    document.getElementById('passta2').style.display="none";
+    document.getElementById('passta3').style.display="none";
+    // --------------------------------------------------
+    document.getElementById('sand1').style.display="none";
+    document.getElementById('sand2').style.display="none";
+    document.getElementById('sand3').style.display="none";
+    // --------------------------------------------------
+    document.getElementById('piza1').style.display="inline-block";
+    document.getElementById('piza2').style.display="inline-block";
+    document.getElementById('piza3').style.display="inline-block";
+    document.getElementById('piza4').style.display="inline-block";
+}
+
+// sand
+function felter3(){
+    document.getElementById('piza1').style.display="none";
+    document.getElementById('piza2').style.display="none";
+    document.getElementById('piza3').style.display="none";
+    document.getElementById('piza4').style.display="none";
+    // --------------------------------------------------
+    document.getElementById('passta1').style.display="none";
+    document.getElementById('passta2').style.display="none";
+    document.getElementById('passta3').style.display="none";
+    // --------------------------------------------------
+    document.getElementById('sand1').style.display="inline-block";
+    document.getElementById('sand2').style.display="inline-block";
+    document.getElementById('sand3').style.display="inline-block";
+}
+
+// pasta
+function felter4(){
+    document.getElementById('piza1').style.display="none";
+    document.getElementById('piza2').style.display="none";
+    document.getElementById('piza3').style.display="none";
+    document.getElementById('piza4').style.display="none";
+    // --------------------------------------------------
+    document.getElementById('sand1').style.display="none";
+    document.getElementById('sand2').style.display="none";
+    document.getElementById('sand3').style.display="none";
+    // --------------------------------------------------
+    document.getElementById('passta1').style.display="inline-block";
+    document.getElementById('passta2').style.display="inline-block";
+    document.getElementById('passta3').style.display="inline-block";
+}
